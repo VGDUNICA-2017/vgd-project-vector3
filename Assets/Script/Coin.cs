406393ed72d3ebf5e5652dd36f3d1354005641e4
sziantoni <stefano.ziantoni20@gmail.com>
2018-02-07 17:38:47 +0100

    Aggiunta Moneta

---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	Rigidbody rb;
+
+
+
+
+	void Awake(){
+
+		rb = GetComponent<Rigidbody> ();
+
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+
+		Destroy (this.gameObject);
+
+
+
+
+
+
+	}
+
+}
