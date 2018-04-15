67f59aafa62f1c7ced60b736a129b660e530504e
Stefano Ziantoni <stefano.ziantoni20@gmail.com>
2018-04-15 19:20:08 +0200

    Menu Principale concluso

--- Assets/Script/MenuPrincipale.cs
+++ Assets/Script/MenuPrincipale.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class MenuPrincipale : MonoBehaviour {
 
+	public GUIStyle stileBottoni1;
 	public GUIStyle stileBottoni;
+
+	public GUIStyle stileBottoni2;
+	public GUIStyle stileBottoni3;
 	private bool menuprincipale;
 	private bool menuOpzioni;
 	private bool caricaPartita;
@@ -19,16 +23,66 @@ public class MenuPrincipale : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUILayout.BeginArea (new Rect (Screen.width / 2 - 200, Screen.height / 2 - 200, 500, 1000));
+
+		GUILayout.BeginArea (new Rect (Screen.width / 2 - 150, Screen.height / 2 - 200, 300, 300));
+
+	
+		if (GUILayout.Button ("Giardino di Harumi", stileBottoni1)) {
+
+			Application.LoadLevel ("Livello2");
+
+		};
+	
+	
+	
+
+	
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (Screen.width / 2 + 150, Screen.height / 2 - 200, 300, 300));
+
+
+
+
+		if(GUILayout.Button ("Borgo di Inuyama", stileBottoni2)){
+
+			Application.LoadLevel ("Level01");
+
+
+		};
+
+
+
+
+
+
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (Screen.width / 2 - 450, Screen.height / 2 - 200, 300, 300));
+
+
+		if (GUILayout.Button ("La Foresta Rossa", stileBottoni3)) {
+
+
+			Application.LoadLevel ("Livello1");
+
+		};
+
+
+
+		GUILayout.EndArea ();
+
+
+		GUILayout.BeginArea (new Rect (Screen.width / 2 - 200, Screen.height / 2 + 150, 500, 500));
 
 		if (menuprincipale) {
-			
 
-			if (GUILayout.Button ("Scegli Livello", stileBottoni)) {
+
+			/**if (GUILayout.Button ("Scegli Livello", stileBottoni)) {
 				menuprincipale = false;
 				caricaPartita = true;
 			
-			};
+			};**/
 
 			if (GUILayout.Button ("Opzioni", stileBottoni)) {
 				menuprincipale = false;
@@ -36,6 +90,7 @@ public class MenuPrincipale : MonoBehaviour {
 			
 			};
 			GUILayout.Button ("Credits", stileBottoni);
+
 			if (GUILayout.Button ("Esci", stileBottoni)) {
 
 				Application.Quit ();
@@ -48,10 +103,10 @@ public class MenuPrincipale : MonoBehaviour {
 		if (menuOpzioni) {
 		
 		
-			GUILayout.Button ("Impostazioni", stileBottoni);
-			GUILayout.Button ("Comandi", stileBottoni);
+		
+			GUILayout.Button ("Audio", stileBottoni);
 			GUILayout.Button ("Difficoltà", stileBottoni);
-			if (GUILayout.Button ("Esci", stileBottoni)) {
+			if (GUILayout.Button ("Indietro", stileBottoni)) {
 
 				menuOpzioni = false;
 				menuprincipale = true;
@@ -65,26 +120,7 @@ public class MenuPrincipale : MonoBehaviour {
 		if (caricaPartita) {
 
 
-			if (GUILayout.Button ("La Foresta Rossa", stileBottoni)) {
-
-				Application.LoadLevel ("Livello1");
-
-			}
-			;
-			if(GUILayout.Button ("Borgo di Inuyama", stileBottoni)){
-
-				Application.LoadLevel ("Livello2");
-
-
-			};
-
-
-			if (GUILayout.Button ("Giardino di Harumi", stileBottoni)) {
-			
-			
-				Application.LoadLevel ("Level01");
-			
-			};
+	
 
 			if (GUILayout.Button ("Indietro", stileBottoni)) {
 
