48279de1b3b12bab303e5236e52c0fc897ce8fa8
Stefano Ziantoni <stefano.ziantoni20@gmail.com>
2018-04-26 21:06:56 +0200

    Altre musiche + modifiche UI

---
+++ Assets/Script/QuadMorteScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadMorteScript : MonoBehaviour {
+
+	public AudioSource death;
+
+	void OnTriggerEnter(){
+	
+		death.Play ();
+	
+	}
+
+}
