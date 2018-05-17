3c89e4957be665e6d06c57d139fb156f4813bc01
s.ziantoni <stefano.ziantoni20@gmail.com>
2018-05-17 16:20:30 +0200

    Modifica UI

--- Assets/Script/HeartGesture.cs
+++ Assets/Script/HeartGesture.cs
@@ -1,67 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeartGesture : MonoBehaviour {
 
-	public   GameObject heartone;
-	public   GameObject hearttwo;
-	public  GameObject hearttree;
+
+
+
+	Text text;
 
 
 	// Use this for initialization
 	void Start () {
-		
-		
+
+		text = GetComponent<Text> ();
+
 	}
-	
+
 	// Update is called once per frame
-	void FixedUpdate () {
-
-		if (PlayerController.currentHealth < 150 && PlayerController.currentHealth >= 100) {
-
-			heartone.SetActive (false);
-			hearttwo.SetActive (true);
-			hearttree.SetActive (true);
-		
-		} else if (PlayerController.currentHealth < 100 && PlayerController.currentHealth >= 50) {
-		
-			heartone.SetActive (false);
-			hearttwo.SetActive (false);
-			hearttree.SetActive (true);
-		} else if (PlayerController.currentHealth < 50) {
-		
-			heartone.SetActive (false);
-			hearttwo.SetActive (false);
-			hearttree.SetActive (false);
-		
-		} else if (PlayerController.currentHealth == 150) {
-		
-			heartone.SetActive (true);
-			hearttwo.SetActive (true);
-			hearttree.SetActive (true);
-		
-		
-		} else {
-
-			heartone.SetActive (false);
-			hearttwo.SetActive (false);
-			hearttree.SetActive (false);
-
-		
-		
-		}
-
+	void Update () {
+		text.text = ""+PlayerController.currentHealth;
 	}
-
-	public  void restart(){
-	
-		heartone.SetActive (true);
-		hearttwo.SetActive (true);
-		hearttree.SetActive (true);
-	
-	
-	}
-
-
 }
